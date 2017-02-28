3feff15e2e7aec50f1208ca94ebea6d2ab967d82
Alexander H. Kneipp <alexander.h.kneipp@gmail.com>
2017-02-28 14:12:57 -0500

    Wrote a shuffle algorithm for deck.
    	Haven't tested it yet though, that'll take some fixes of some certain
    	janky code

--- src/poker/Deck.java
+++ src/poker/Deck.java
@@ -6,8 +6,31 @@
  */
 package poker;
 
+import java.util.Random;
+
 public class Deck
 {
-
+	/**
+	 * Uses the Fisher-Yates shuffle algorithm to shuffle the deck
+	 */
+	private void shuffle()//TODO test.
+	{
+		Card[] tempArr = new Card[52];
+		System.arraycopy(this.cards, 0, tempArr, 0, 52);
+		for(int i = 0; i < 52; i++)
+		{
+			int k = shuffler.nextInt(52-i);
+			for(int j = 0; j < 52-i-k; j++)
+			{
+				if(tempArr[k + j] != null)
+				{
+					this.cards[i] = tempArr[k+j];
+					tempArr[k + j] = null;
+					break;
+				}
+			}
+		}
+	}
     private Card[] cards= new Card[52];
+    private Random shuffler = new Random();
 }
