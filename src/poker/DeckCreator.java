450ff6653559e7c42e22c4f80028b28ddf3188c9
Alex Kneipp <alexander.h.kneipp@gmail.com>
2017-02-28 00:01:32 -0500

    Added header comments to all current files

--- src/poker/DeckCreator.java
+++ src/poker/DeckCreator.java
@@ -1,15 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Poker game project for AP Computer Science.
+ * Written by:
+ * Dylan Poole and
+ * Alex Kneipp
  */
 package poker;
 /**
- * 
- */
-
-/**
- * @author Dylan
+ * @author Dylan Poole
  *
  */
 public class DeckCreator {
